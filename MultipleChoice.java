9849eec11502663092aa08a48c9bf4471e9bae65
AMADA-MANUEL <manuelamada707@gmail.com>
2026-02-07 19:11:48 +0200

    question quiz ?

---
+++ MultipleChoice.java
@@ -0,0 +1,25 @@
+public class MultipleChoice {
+    public static void main (String[] args) {
+        System.out.println("How many legs do spiders have ?");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("What was the historic event tha happend in South Africa In The year 2026?");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        System.out.println("What historic moment happend inthe markets in the year 2026 January");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+
+}
