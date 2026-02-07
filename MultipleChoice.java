5661749930764d18838597d5aa06c6b9476fb0e8
AMADA-MANUEL <manuelamada707@gmail.com>
2026-02-07 19:39:35 +0200

    scanner

--- MultipleChoice.java
+++ MultipleChoice.java
@@ -7,25 +7,25 @@ public class MultipleChoice {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("How many legs do spiders have ?");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
+        System.out.println("a) 7");
+        System.out.println("b) 6");
+        System.out.println("c) 8");
         System.out.println();
 
         System.out.println("What was the historic event tha happend in South Africa In The year 2026?");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("a) World cup");
+        System.out.println("b) World Economic Forum");
+        System.out.println("c) G20 Summit");
 
 
 
-        System.out.println("What historic moment happend inthe markets in the year 2026 January");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
+        System.out.println("What historic moment happend in the markets in the year 2026 January");
+        System.out.println("a Gold hit 5600)");
+        System.out.println("b The dollar hit R21)");
+        System.out.println("cThe dollar hit R15 to Rands)");
+
 
 
         scan.next();
